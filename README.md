ee66fbf52f9bfdcfb6c0ce3326c7f5c7d105e177
engrmohsinyaseen <47355064+engrmohsinyaseen@users.noreply.github.com>
2019-10-21 17:11:54 +0200

    create react app

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-# react-app-algolia
-This project use Algolia Places that provides a fast, distributed and easy way to use address search autocomplete JavaScript library on website. 
 # places
 
-_This project was generated with [Algolia](https://algolia.com)._
+_This project was generated with (https://github.com/algolia/create-instantsearch-app) by [Algolia](https://algolia.com)._
 
 ## Get started
 
 To run this project locally, install the dependencies and run the local server:
 
 npm install 
-
 npm start
-
 
---
+++ src/App.js
@@ -0,0 +1,57 @@
+import algoliasearch from 'algoliasearch/lite';
+import React, { Component, Fragment } from 'react';
+import { InstantSearch } from 'react-instantsearch-dom';
+import {
+  GoogleMapsLoader,
+  GeoSearch,
+  Marker,
+  Control,
+} from 'react-instantsearch-dom-maps';
+import Places from './places/widget';
+import './App.css';
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ais-InstantSearch">
+        <h3>React App InstantSearch Places</h3>
+        <InstantSearch indexName="airports" searchClient={searchClient}>
+          <div className="search-panel">
+            <div className="search-panel__results">
+              <Places
+                defaultRefinement={{
+                  lat: 37.7793,
+                  lng: -122.419,
+                }}
+              />
+
+              <div style={{ height: 500 }}>
+                <GoogleMapsLoader apiKey="AIzaSyBawL8VbstJDdU5397SUX7pEt9DslAwWgQ">
+                  {google => (
+                    <GeoSearch google={google}>
+                      {({ hits }) => (
+                        <Fragment>
+                          <Control />
+                          {hits.map(hit => (
+                            <Marker key={hit.objectID} hit={hit} />
+                          ))}
+                        </Fragment>
+                      )}
+                    </GeoSearch>
+                  )}
+                </GoogleMapsLoader>
+              </div>
+            </div>
+          </div>
+        </InstantSearch>
+      </div>
+    );
+  }
+}
+
+export default App;
